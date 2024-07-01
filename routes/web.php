3b80dfc6528ded2a00dccf2193c517f57e776bdf
Axelcichello <csaxel49@gmail.com>
2024-07-01 20:00:42 -0300

    auth lista

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Backpack;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\BackpackController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\BackofficeController;
 
 //User
@@ -11,7 +13,7 @@ Route::get('/', [BackpackController::class, 'index']);
 Route::get('backpack/{id}', [BackpackController::class, 'backpack']);
 Route::get('categories/{id}', [BackpackController::class, 'list']);
 Route::post('cart/add', [CartController::class, 'add'])->name('cart.add');
-Route::get('checkout', [CartController::class, 'check']);
+Route::get('checkout', [CartController::class, 'check'])->middleware('auth');
 Route::post('cart/buy', [CartController::class, 'buy'])->name('cart.buy');
 Route::get('thanks', [CartController::class, 'thanks']);
 
@@ -24,3 +26,12 @@ Route::get('/backoffice/crear', [BackofficeController::class, 'crear']);
 
 Route::get('backoffice/{id}/editar', [BackofficeController::class, 'editar']);
 Route::post('backoffice/{id}', [BackofficeController::class, 'update']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    // Login
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [LoginController::class, 'login']);
+});
